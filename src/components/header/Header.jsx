7bb2fac847eb9a0bb1b92663724aa5ce6458f983
mbozkayaGitHub <mbozkaya27@gmail.com>
2023-07-23 22:08:13 +0300

    header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
 
       <div className="headerRigth">
         <h3>
-          <span class="steve">BOZKAYA </span>
+          <span class="mike">BOZKAYA </span>
           Çorba Salonu
         </h3>
 <br />        <h3 class="mesai">Gece 2 ye kadar açıktır</h3>
